f0b23f18bd332c9ee6340ae2d86b42aaa1df88f1
njoguhmureithi <77837735+njoguhmureithi@users.noreply.github.com>
2021-09-27 16:31:11 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(0, x):
@@ -9,3 +9,4 @@ def safe_print_list(my_list=[], x=0):
             pass
     print()
     return count
+
